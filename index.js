9701f22ce154053e1d271ced6e0b7550f820686f
cash-i1 <cashignitro@gmail.com>
2024-02-26 21:00:09 +1100

    added flying ignitro

---
+++ index.js
@@ -0,0 +1,7 @@
+var elements = document.querySelectorAll('.flying-ignitro');
+
+elements.forEach(function(element) {
+    element.addEventListener('click', function() {
+        console.log("lerbenGames")
+    });
+});
