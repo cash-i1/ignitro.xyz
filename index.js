283aa040e03491b42181fa5cc2f0a7b7ce14f7f0
cash-i1 <cashignitro@gmail.com>
2024-02-26 21:34:24 +1100

    added bad button css

--- index.js
+++
@@ -1,7 +0,0 @@
-var elements = document.querySelectorAll('.flying-ignitro');
-
-elements.forEach(function(element) {
-    element.addEventListener('click', function() {
-        console.log("lerbenGames")
-    });
-});
