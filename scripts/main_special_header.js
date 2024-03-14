6290c6137a6844b4225e8f4e56b6c933fe57f9b6
cash-i1 <cashignitro@gmail.com>
2024-03-14 18:01:32 +1100

    removed old stuff

--- scripts/main_special_header.js
+++
@@ -1,4 +0,0 @@
-window.onload = function() {
-    document.querySelector("#main_special_header").innerHTML = window.location.origin.replace(/^https?:\/\//, '').replace(/\//g, '');
-    console.log(window.location.origin)
-}
