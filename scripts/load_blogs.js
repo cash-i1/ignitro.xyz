89ce4c0f6c46dfaa4c7d2e3ec49a74bf143bf346
cash-i1 <cashignitro@gmail.com>
2024-06-22 18:59:37 +1000

    reveres blog adding so its most recent and added new blog and post date

--- scripts/load_blogs.js
+++ scripts/load_blogs.js
@@ -4,7 +4,7 @@ function add_blogs() {
   })
     .then((response) => response.json())
     .then((data) => {
-      for (let post of data["posts"]) {
+      for (let post of data["posts"].reverse()) {
         let blogs_div = document.getElementById("blogs");
 
         let title_header = document.createElement("h2");
@@ -14,11 +14,16 @@ function add_blogs() {
         body_para.innerHTML= post.body;
 
         let post_div = document.createElement("div");
-
+            let date_para = document.createElement("p");
+              date_para.innerHTML = "postation date: " + post.date;
+                date_para.style = "color: grey; font-size: 10px"
         post_div.appendChild(title_header);
+        post_div.appendChild(date_para);
+
         post_div.appendChild(body_para);
 
         blogs_div.appendChild(post_div);
+          break;
       }
     })
     .catch((error) => console.error("Error:", error));
